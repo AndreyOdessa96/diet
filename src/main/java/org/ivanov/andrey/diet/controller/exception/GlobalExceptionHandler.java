017d55404fd41d93d604423025bd63f5e02a7ec0
amartynov <amartynov@lohika.com>
2025-03-27 21:41:22 +0200

    Bts 9 add exception handler

--- src/main/java/org/ivanov/andrey/diet/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ivanov/andrey/diet/controller/exception/GlobalExceptionHandler.java
@@ -6,20 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.ivanov.andrey.diet.controller.exception.ErrorCode.INTERNAL_ERROR;
 import static org.ivanov.andrey.diet.controller.exception.ErrorCode.INVALID_PARAMS;
+import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private static final Logger LOG = getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorResponse> handleConstraintViolation(ConstraintViolationException ex) {
+        LOG.error("Error while request", ex);
         String message = ex.getConstraintViolations()
                 .stream()
                 .findFirst()
@@ -32,7 +33,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleAnyException(Exception ex) {
-        logger.error("Unexpected error occurred", ex);
+        LOG.error("Unexpected error occurred", ex);
 
         ErrorResponse error = new ErrorResponse(INTERNAL_ERROR, "Unexpected error: " + ex.getMessage());
         return new ResponseEntity<>(error, INTERNAL_SERVER_ERROR);
